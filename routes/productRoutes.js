3c00a794c1d3874f9ce122d1efe9552da435d29c
Burhan Maseel <burhanmaseel@users.noreply.github.com>
2019-04-09 21:43:45 +0500

    Product Routes

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,10 +3,10 @@ module.exports = app => {
 
     app.route("/api/addProduct").post(productControllers.addProduct);
     app.route("/api/fetchAllProducts").get(productControllers.fetchAllProducts);
-    app.route("/api/deleteProduct").delete(productControllers.deleteProduct);
     app.route("/api/fetchProducts").get(productControllers.fetchProductsByUser);
     app.route("/api/fetchSingleProductDetails").get(productControllers.fetchSingleProductDetails);
     app.route("/api/fetchProductsByAlbums").get(productControllers.fetchProductsByAlbums);
     app.route("/api/updateProduct").put(productControllers.updateProduct);
+    app.route("/api/deleteProduct").delete(productControllers.deleteProduct);
 
 };
