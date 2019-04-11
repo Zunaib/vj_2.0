c0116e1ccbb8b1c732cc6884b0ccb37fc22e83bf
Burhan Maseel <burhanmaseel@gmail.com>
2019-04-11 17:29:23 +0500

    User Settings

--- controllers/userSettingsControllers.js
+++ controllers/userSettingsControllers.js
@@ -19,8 +19,42 @@ exports.changeSettings = (req, res) => {
     });
 };
 
-exports.changeGeneralBio = (req, res) => {
-    Object.keys(req.body).forEach((key) => {
-        console.log(testkey);
-    })
+exports.changeGeneralBioDesigner = (req, res) => {
+    const { generalBioDesigner } = req.body;
+    Users.findByIdAndUpdate(req.user.id, { generalBioDesigner: generalBioDesigner })
+        .then(user => res.status(200).json({ user: user, success: true }))
+        .catch(err => res.status(400).json({ err: err, success: false }));
 }
+
+exports.changeGeneralBioBlogger = (req, res) => {
+    const { generalBioBlogger } = req.body;
+    Users.findByIdAndUpdate(req.user.id, { generalBioBlogger: generalBioBlogger })
+        .then(user => res.status(200).json({ user: user, success: true }))
+        .catch(err => res.status(400).json({ err: err, success: false }));
+}
+
+exports.changeGeneralBioVlogger = (req, res) => {
+    const { generalBioVlogger } = req.body;
+    Users.findByIdAndUpdate(req.user.id, { generalBioVlogger: generalBioVlogger })
+        .then(user => res.status(200).json({ user: user, success: true }))
+        .catch(err => res.status(400).json({ err: err, success: false }));
+}
+
+exports.fetchGeneralBioDesigner = (req, res) => {
+    Users.findById(req.user.id)
+        .then(user => res.status(200).json({ generalBioDesigner: user.generalBioDesigner, success: true }))
+        .catch(err => res.status(400).json({ err: err, success: false }));
+}
+
+exports.fetchGeneralBioBlogger = (req, res) => {
+    Users.findById(req.user.id)
+        .then(user => res.status(200).json({ generalBioBlogger: user.generalBioBlogger, success: true }))
+        .catch(err => res.status(400).json({ err: err, success: false }));
+}
+
+exports.fetchGeneralBioVlogger = (req, res) => {
+    Users.findById(req.user.id)
+        .then(user => res.status(200).json({ generalBioVlogger: user.generalBioVlogger, success: true }))
+        .catch(err => res.status(400).json({ err: err, success: false }));
+}
+
--- routes/userSettingsRoutes.js
+++ routes/userSettingsRoutes.js
@@ -2,6 +2,12 @@ module.exports = app => {
     const userSettingsControllers = require("../controllers/userSettingsControllers");
 
 
-    app.route("/api/changeGeneralBio").get(userSettingsControllers.changeGeneralBio);
+    app.route("/api/changeSettings").post(userSettingsControllers.changeSettings);
+    app.route("/api/changeGeneralBioDesigner").get(userSettingsControllers.changeGeneralBioDesigner);
+    app.route("/api/changeGeneralBioBlogger").get(userSettingsControllers.changeGeneralBioBlogger);
+    app.route("/api/changeGeneralBioVlogger").get(userSettingsControllers.changeGeneralBioVlogger);
+    app.route("/api/fetchGeneralBioDesigner").get(userSettingsControllers.fetchGeneralBioDesigner);
+    app.route("/api/fetchGeneralBioBlogger").get(userSettingsControllers.fetchGeneralBioBlogger);
+    app.route("/api/fetchGeneralBioVlogger").get(userSettingsControllers.fetchGeneralBioVlogger);
   };
   
