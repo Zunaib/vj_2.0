3bdb3cb74086794403ee634e8c13a12021619240
Burhan Maseel <burhanmaseel@users.noreply.github.com>
2019-06-16 03:54:31 +0500

    Search Module Optimized According to Products Blogs Vlogs Users

--- controllers/searchControllers.js
+++ controllers/searchControllers.js
@@ -3,45 +3,67 @@ const Vlogs = require("../models/vlogs");
 const Products = require("../models/products");
 const Blogs = require("../models/blogs");
 
-exports.search = (req, res) => {
+exports.search = async (req, res) => {
   let { queryString } = req.body;
-  if (req.body.queryString) {
-    const regex = new RegExp(escapeRegex(queryString), "gi");
-    Users.find({
-      deletedAt: null,
-      $or: [{ firstName: regex }, { lastName: regex }]
-    })
-      .select("firstName lastName email displayPicture")
-      .lean()
-      .then(users =>
-        res.status(200).json({
-          success: true,
-          users: users,
-          message: "Users Searched Successfully"
-        })
-      )
-      .catch(err =>
-        res
-          .status(400)
-          .json({ success: false, message: "Something went wrong" })
-      );
-  } else {
-    Users.find({ deletedAt: null })
-      .select("firstName lastName email displayPicture")
-      .lean()
-      .then(users =>
-        res.status(200).json({
-          success: true,
-          users: users,
-          message: "Users Searched Successfully"
-        })
-      )
-      .catch(err =>
-        res
-          .status(400)
-          .json({ success: false, message: "Something went wrong" })
-      );
-  }
+  const regex = new RegExp(escapeRegex(queryString), "gi");
+
+  let users = [];
+  let products = [];
+  let blogs = [];
+  let vlogs = [];
+
+  await Users.find({
+    deletedAt: null,
+    $or: [{ firstName: regex }, { lastName: regex }]
+  })
+    .select("firstName lastName email displayPicture")
+    .lean()
+    .then(foundUsers => (users = foundUsers))
+    .catch(err =>
+      res.status(400).json({ success: false, message: "Something went wrong" })
+    );
+
+  await Blogs.find({
+    deletedAt: null,
+    $or: [{ title: regex }, { content: regex }]
+  })
+    .select("title description content likes comments")
+    .lean()
+    .then(foundBlogs => (blogs = foundBlogs))
+    .catch(err =>
+      res.status(400).json({ success: false, message: "Something went wrong" })
+    );
+
+  await Vlogs.find({
+    deletedAt: null,
+    $or: [{ title: regex }, { description: regex }]
+  })
+    .select("title description videoLink likes comments")
+    .lean()
+    .then(foundVlogs => (vlogs = foundVlogs))
+    .catch(err =>
+      res.status(400).json({ success: false, message: "Something went wrong" })
+    );
+
+  await Products.find({
+    deletedAt: null,
+    $or: [{ productName: regex }, { description: regex }]
+  })
+    .select("productName description price discount likes comments")
+    .lean()
+    .then(foundProducts => (products = foundProducts))
+    .catch(err =>
+      res.status(400).json({ success: false, message: "Something went wrong" })
+    );
+
+  res.status(200).json({
+    success: true,
+    users: users,
+    blogs: blogs,
+    vlogs: vlogs,
+    products: products,
+    message: "Items Searched Successfully"
+  });
 };
 
 escapeRegex = text => {
