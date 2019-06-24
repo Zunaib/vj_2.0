d3e8f3a88338caa6371010020bf132c3877481ed
Burhan Maseel <burhanmaseel@gmail.com>
2019-06-24 23:33:51 +0500

    Admin Month Vise Revenues Fetched

--- routes/admin/productDashboardRoutes.js
+++ routes/admin/productDashboardRoutes.js
@@ -1,3 +1,11 @@
 module.exports = app => {
   const productDashboardControllers = require("../../controllers/admin/productDashboardControllers");
+
+  app
+    .route("/api/admin/fetchMonthViseRevenue")
+    .get(productDashboardControllers.fetchMonthViseRevenue);
+
+  app
+    .route("/api/admin/fetchPopularProducts")
+    .get(productDashboardControllers.fetchPopularProducts);
 };
