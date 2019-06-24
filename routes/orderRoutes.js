ed1c43fe455b386198237a3816f958d166f86376
Burhan Maseel <burhanmaseel@users.noreply.github.com>
2019-06-24 20:37:52 +0500

    Order Status Change API implemented

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -4,7 +4,7 @@ module.exports = app => {
   app.route("/api/placeOrder").post(orderControllers.placeOrder);
   app.route("/api/fetchCustomerOrders").get(orderControllers.fetchCustomerOrders);
   app.route("/api/fetchDesignerOrders").get(orderControllers.fetchDesignerOrders);
-  app.route("/api/cancelOrderProductByCustomer").post(orderControllers.cancelOrderProductByCustomer);
+  app.route("/api/changeOrderStatus").post(orderControllers.changeOrderStatus);
 
   
 };
